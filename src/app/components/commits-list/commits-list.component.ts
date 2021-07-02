88fd074dfd38a93e48ef7cf67c594d0556453658
Fausto Posso <faustopossopaz@gmail.com>
2021-07-02 18:54:08 -0500

    add service to call API

--- src/app/components/commits-list/commits-list.component.ts
+++ src/app/components/commits-list/commits-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from './../../services/github.service';
 
 @Component({
   selector: 'app-commits-list',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commits-list.component.scss'],
 })
 export class CommitsListComponent implements OnInit {
+  commitsList: Array<any> = [];
 
-  constructor() { }
+  constructor(private githubService: GithubService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getCommitsList();
+  }
+
+  getCommitsList() {  
+    this.githubService.getCommitsList().subscribe( 
+      (data:any) => {
+        this.commitsList = data;
+        console.log(this.commitsList)
+
+      },
+      (err) => {
+       console.log(err)
+       alert(err.error.message)
+      })  
+  }
 
 }
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor(private http: HttpClient) { }
+
+    getCommitsList(): Observable<any>{
+      const url = `https://api.github.com/repos/${environment.user}/${environment.repo}/commits`;
+      return this.http.get<any>(url);    
+    }
+}
